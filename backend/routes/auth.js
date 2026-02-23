58fdb9f7d8e8ce1475b6343b430f8d5b1fd2321c
unknown <faheem.shiledar@gmail.com>
2026-02-23 17:11:02 +0530

    Made healthcard public route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,6 +5,6 @@ const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/me', protect, getMe);
+router.get('/me', getMe);
 
 module.exports = router;
